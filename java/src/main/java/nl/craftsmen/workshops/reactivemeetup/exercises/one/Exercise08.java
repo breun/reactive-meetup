e1e4e5720a190dd6e1393dc64e8c12abbacc8363
Nils Breunese <nils@breun.nl>
2016-12-19 19:33:14 +0100

    Exercise 1.8.

--- java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/one/Exercise08.java
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/one/Exercise08.java
@@ -28,7 +28,10 @@ public class Exercise08 {
 		//
 		// HINT: To bake the cake, use the bake() method of the Cake class.
 		
-		Observable<Cake> cake$ = unknown(); // ???
+		Observable<Cake> cake$ = ingredient$
+			.filter(ingredient -> IngredientType.FRUIT.equals(ingredient.getType()))
+			.reduce(new Cake("Fruitcake"), Cake::addIngredient)
+			.map(Cake::bake);
 		
 		// If you've followed the recipe then you should end up with one baked delicious and healthy strawberry pineapple cake.
 		
