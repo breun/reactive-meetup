79ce84ae1dedf1a6c7ac79ec8d25465239874906
Nils Breunese <nils@breun.nl>
2016-12-19 19:32:51 +0100

    Exercise 1.4.

--- java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/one/Exercise04.java
+++ java/src/main/java/nl/craftsmen/workshops/reactivemeetup/exercises/one/Exercise04.java
@@ -3,6 +3,7 @@ package nl.craftsmen.workshops.reactivemeetup.exercises.one;
 import static nl.craftsmen.workshops.reactivemeetup.util.Utils.*;
 
 import nl.craftsmen.workshops.reactivemeetup.util.ExampleStreams;
+import nl.craftsmen.workshops.reactivemeetup.util.Utils;
 import rx.Observable;
 
 public class Exercise04 {
@@ -14,7 +15,7 @@ public class Exercise04 {
 		//
 		// HINT: You can make use of the utility function Utils.isPrime to check if a given number is a prime number.
 		
-		Observable<Integer> primeNumber$ = unknown(); // ???
+		Observable<Integer> primeNumber$ = number$.filter(Utils::isPrime);
 		
 		// If implemented correctly, the application will output the following numbers: 1, 7, 2, 2, 7, 3
 		
